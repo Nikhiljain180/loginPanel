e9aab6ad591807cfad17ac751d4e8b6258025a84
Nikhil Jain <nikhil.jain@bonavitatech.com>
2016-11-11 00:47:09 +0530

    login panel

--- public/javascripts/chirpApp.js
+++ public/javascripts/chirpApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('chirpApp', ['ngRoute', 'ngResource']).run(function($rootScope) {
+var app = angular.module('yusome', ['ngRoute', 'ngResource']).run(function($rootScope) {
 	$rootScope.authenticated = false;
 	$rootScope.current_user = '';
 	
@@ -22,7 +22,7 @@ app.config(function($routeProvider){
 			controller: 'authController'
 		})
 		//the signup display
-		.when('/register', {
+		.when('/signup', {
 			templateUrl: 'register.html',
 			controller: 'authController'
 		});
